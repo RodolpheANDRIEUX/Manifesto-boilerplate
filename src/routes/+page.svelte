bf002f33f555a2d3d4bc5b0baaf3a27955308e35
Rodolphe <rodolpheandrieux@gmail.com>
2024-10-27 17:05:47 +0100

    Added the base

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,18 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<svelte:head>
+    <title>Boilerplate</title>
+    <meta name="description" content="A very simple boirlerplate for sveltekit" />
+
+    <!-- SEO -->
+    <meta name="keywords" content="keyword1, keyword2, keyword3" />
+    <meta name="robots" content="index, follow" />
+    <!-- Open Graph tags for social media platforms -->
+    <meta property="og:title" content="Title of your site or page" />
+    <meta property="og:description" content="Description for social media sharing" />
+    <meta property="og:image" content="URL to your preview image" />
+    <meta property="og:url" content="URL of your site or page" />
+    <meta property="og:type" content="website" />
+</svelte:head>
+
+
+<h1>Welcome to my SvelteKit boilerplate</h1>
+<p>Visit <a href="https://portfolio-rodolphe-andrieux.vercel.app/">my portfolio</a></p>
